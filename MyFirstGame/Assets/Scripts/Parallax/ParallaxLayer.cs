9e8e044d6165e137bfc8036a18cac18105504fc5
SmokedKoala <k.azarov@inbox.ru>
2021-07-09 18:31:04 +0300

    add: camera controll and inventory
    link for paralax ( camera scripts) - https://answers.unity.com/questions/551808/parallax-scrolling-using-orthographic-camera.html

---
+++ MyFirstGame/Assets/Scripts/Parallax/ParallaxLayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+ public class ParallaxLayer : MonoBehaviour
+ {
+       public float parallaxFactor;
+       public void Move(float delta)
+       {
+           Vector3 newPos = transform.localPosition;
+           newPos.x -= delta * parallaxFactor;
+           transform.localPosition = newPos;
+       }
+ }
+ 
